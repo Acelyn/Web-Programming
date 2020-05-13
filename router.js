058ca4393f433401dbf7025102c40a38366e7e2f
Acelyn <Acelyn@users.noreply.github.com>
2020-05-12 22:48:55 -0400

    git /

--- router.js
+++ router.js
@@ -12,68 +12,6 @@ router.get('/', function(request, response) {
   response.render('index');
 });
 
-//
-router.get('/signup',function(request, response) {
-  response.render('signup');
-});
-
-// Handle login requests/
-router.post('/login', users.login);
-
-// Handle sign up requests /
-router.post('/users',users.create);
-
-// Handle logout requests
-router.get('/logout', function(request, response) {
-  request.session.user = undefined;
-  response.redirect('/');
-});
-/*
-// Check for admin status
-const authorize = function(request, response, next) {
-  if (request.session.admin) {
-    console.log("sile")
-    next(); // Fulfill the request
-  } else {
-    response.status(401).end();
-  }
-};
-*/
-// Handle club requests
-
-router.get('/clubs', clubs.index);
-router.get('/clubs/new', clubs.new);
-
-router.get('/clubs/:id', clubs.retrieve);
-
-// redirect when user click their club on the account page
-router.get('/clubs/club._id', clubs.retrieve);
-// router.get('/clubs/:category',clubs.retreve);
-
-// Create a new Club
-router.post('/clubs', clubs.create);
-
-// add new member to the clubs
-router.put('/clubs/:id/',clubs.newMember);
-
-// Update Club information
-router.put('/clubs/:id', clubs.update);
-
-// Delete a Club
-router.delete('/clubs/:id',clubs.delete);
-
-router.put('/clubs',clubs.retrieve);
-
-// add new member to the clubs
-router.put('/clubs/:id/',clubs.newMember);
-
-
-
-// Handle user requests
-router.get('/users/new', users.new);
-router.get('/users', users.index);
-// router.post('/users', users.index);
-
 
 // Export the router
 module.exports = router;
